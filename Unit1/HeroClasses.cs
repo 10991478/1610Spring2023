d1dd0c26a5bf491b80e0453c252587cced1d1bfd
10991478 <122633451+10991478@users.noreply.github.com>
2023-01-23 23:04:30 -0700

    Added function & variable to Hero class & made new Hero object

--- Unit1/HeroClasses.cs
+++ Unit1/HeroClasses.cs
@@ -6,6 +6,7 @@ public class Program
 {
 	public Hero heroOne;
 	public Hero heroTwo;
+	public Hero heroThree;
 	
 	public void Main()
 	{
@@ -23,10 +24,21 @@ public class Program
 		
 		Console.WriteLine(heroTwo.health);
 		Console.WriteLine(heroTwo.powerLevel);
+		
+		heroThree.health = 69;
+		heroThree.powerLevel = 7;
+		heroThree.name = "Jojo";
+		Console.WriteLine(heroThree.name + " has " + heroThree.health + " health and is level " + heroThree.powerLevel + ".");
+		heroThree.die();
+		Console.WriteLine(heroThree.name + " has " + heroThree.health + " health. " + heroThree.name + " is dead.");
 	}
 }
 
 public class Hero {
 	public int health;
 	public int powerLevel;
+	public string name;
+	public void die(){
+		this.health = 0;
+	}
 }
