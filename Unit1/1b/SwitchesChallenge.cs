78d34b6d4e34ab1796a3675a1bf4945f9ad80db8
Theta Rich <10991478@uvu.edu>
2023-01-28 15:29:56 -0700

    Created a couple Attack objects and a Pikachu object

--- Unit1/1b/SwitchesChallenge.cs
+++ Unit1/1b/SwitchesChallenge.cs
@@ -1,24 +1,28 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
+    Attack thunderThighs = new Attack("thunder thighs", "electric", 30);
+    Attack headButt = new Attack("head butt", "normal", 15);
+    Attack lightningPunch = new Attack("lightning punch", "electric", 40);
+    Pikachu myPikachu = new Pikachu();
+    public static void Main (string[] args) {
     
-  }
+    }
 }
 
 class Pikachu{
-  public int health = 100;
-  public string name;
-  Attack[] attacks;
+    public int health = 100;
+    public string name;
+    Attack[] attacks;
 }
 
 class Attack{
-  public string name;
-  public string type;
-  public int damage;
-  public Attack(string n, string t, int d){
-    this.name = n;
-    this.type = t;
-    this.damage = d;
+    public string name;
+    public string type;
+    public int damage;
+    public Attack(string n, string t, int d){
+        this.name = n;
+        this.type = t;
+        this.damage = d;
   }
 }
