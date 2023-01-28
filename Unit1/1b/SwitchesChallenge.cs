6d48033a46e27167a298d7bf621ca70d12fb6bbf
10991478 <122633451+10991478@users.noreply.github.com>
2023-01-28 15:17:27 -0700

    created Pikachu and Attack classes

--- Unit1/1b/SwitchesChallenge.cs
+++ Unit1/1b/SwitchesChallenge.cs
@@ -1,1 +1,24 @@
+using System;
 
+class Program {
+  public static void Main (string[] args) {
+    
+  }
+}
+
+class Pikachu{
+  public int health = 100;
+  public string name;
+  Attack[] attacks;
+}
+
+class Attack{
+  public string name;
+  public string type;
+  public int damage;
+  public Attack(string n, string t, int d){
+    this.name = n;
+    this.type = t;
+    this.damage = d;
+  }
+}
