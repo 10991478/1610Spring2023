a4079411f64bfe2f4c996a78e4c0b7d51ce2ce17
Theta Rich <10991478@uvu.edu>
2023-01-28 15:59:00 -0700

    fixed some errors and gave myPikachu some attacks

--- Unit1/1b/SwitchesChallenge.cs
+++ Unit1/1b/SwitchesChallenge.cs
@@ -1,19 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program {
-    Attack thunderThighs = new Attack("thunder thighs", "electric", 30);
-    Attack headButt = new Attack("head butt", "normal", 15);
-    Attack lightningPunch = new Attack("lightning punch", "electric", 40);
-    Pikachu myPikachu = new Pikachu();
+    static Attack thunderThighs = new Attack("thunder thighs", "electric", 30);
+    static Attack headButt = new Attack("head butt", "normal", 15);
+    static Attack lightningPunch = new Attack("lightning punch", "electric", 40);
+    static Pikachu myPikachu = new Pikachu();
     public static void Main (string[] args) {
-    
+        Console.WriteLine("What would you like your Pikachu's name to be?");
+        myPikachu.name = Console.ReadLine();
+        myPikachu.attacks.Add(thunderThighs);
+        myPikachu.attacks.Add(headButt);
+        myPikachu.attacks.Add(lightningPunch);
     }
 }
 
 class Pikachu{
     public int health = 100;
     public string name;
-    Attack[] attacks;
+    public List<Attack> attacks = new List<Attack>();
 }
 
 class Attack{
