b6bc2f395cec655cd20fcce1fac4bba3af22d16e
Theta Rich <10991478@uvu.edu>
2023-02-19 14:54:36 -0700

    commented out following + Awake position cause it's busted

--- Unit 2/Unit2DGMProj/Assets/followCylander.cs
+++ Unit 2/Unit2DGMProj/Assets/followCylander.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class followCylander : MonoBehaviour
 {
     void Awake(){
+        /*
         transform.position.y = 6;
         transform.position.x = 3;
         transform.position.z = 2;
         print("position set");
+        */
     }
     // Start is called before the first frame update
     void Start()
@@ -19,11 +21,13 @@ public class followCylander : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if(character.transform.position.y < transform.position.y - 1){
             transform.position.y -= .2;
         }
         else if (character.transform.position.y > transform.position.y + 1){
             transform.position.y += .2;
         }
+        */
     }
 }
