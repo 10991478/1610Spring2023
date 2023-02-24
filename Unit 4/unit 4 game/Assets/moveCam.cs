12b70bc12d33adc3ac8977ba6324dfc7da837f2d
Theta Rich <10991478@uvu.edu>
2023-02-23 17:03:08 -0700

    fixed camera following stuff

--- Unit 4/unit 4 game/Assets/moveCam.cs
+++ Unit 4/unit 4 game/Assets/moveCam.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class moveCam : MonoBehaviour
 {
     public float sensativity;
+    public float followDistance;
     public GameObject target;
 
     void Update()
     {
+        transform.LookAt(target.transform.position);
+        float dist = Vector3.Distance(target.transform.position, transform.position);
+        if (dist > followDistance){
+            transform.Translate(Vector3.forward * dist * Time.deltaTime);
+        }
         float movement = Input.GetAxis("Mouse X");
         float xmove = movement * sensativity;
         movement = Input.GetAxis("Mouse Y");
         float ymove = movement * sensativity;
+        transform.RotateAround(target.transform.position, Vector3.right, ymove * Time.deltaTime * sensativity);
         transform.RotateAround(target.transform.position, Vector3.up, xmove * Time.deltaTime * sensativity);
-        transform.RotateAround(Vector.zero, transform.right, ymove * Time.deltaTime * sensativity);
-        
     }
 }
