618171bba08b31dfbd4ea5dbf7b521cfebfc64aa
Theta Rich <10991478@uvu.edu>
2023-03-10 15:18:03 -0700

    added laser + laser mat + movement script for player

--- Unit 6/UFO TopDown/Assets/Scripts/PlayerController.cs
+++ Unit 6/UFO TopDown/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 25;
+    private float horizontalInput;
+    public float xbound = 25;
+    public Transform blaster;
+    public GameObject lazerBolt;
+    
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        if (transform.position.x < -xbound){
+            transform.position = new Vector3(-xbound, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xbound){
+            transform.position = new Vector3(xbound, transform.position.y, transform.position.z);
+        }
     }
 }
