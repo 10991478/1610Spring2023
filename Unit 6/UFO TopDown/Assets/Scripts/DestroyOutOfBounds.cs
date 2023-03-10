f4ef7ae129c7a6c01cfddbb193aa412f6ddef07b
Theta Rich <10991478@uvu.edu>
2023-03-10 15:56:52 -0700

    created destroyoutofbounds script and applied to laser

---
+++ Unit 6/UFO TopDown/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float upperBound = 20.0f;
+    public float lowerBound = -10.0f;
+    void Update()
+    {
+        if (transform.position.z > upperBound){
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound){
+            Destroy(gameObject);
+        }
+    }
+}
