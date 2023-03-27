17e5a098f526c30d7b46c03d703b2b7401f290e0
Theta Rich <10991478@uvu.edu>
2023-03-27 16:42:20 -0600

    PickupSpawnManager now spawns pickups w boltCount and fireRate in mind
    Stops spawning BoltPickup if max bolt count is reached and stops spawning FireRatePickup if min fire rate is reached

--- Unit 6/UFO TopDown/Assets/Scripts/PickupSpawnManager.cs
+++ Unit 6/UFO TopDown/Assets/Scripts/PickupSpawnManager.cs
@@ -9,17 +9,56 @@ public class PickupSpawnManager : MonoBehaviour
     private float spawnPosZ = 20f;
     private float startDelay = 6f;
     private float spawnInterval = 5f;
-
+    public IntObj boltCount;
+    public FloatScriptableObject fireRate;
+    private bool hasBoltBeenRemoved;
+    private bool hasFireBeenRemoved;
+    private List<GameObject> validPickups;
 
     void Start()
     {
         InvokeRepeating("SpawnRandomPickup", startDelay, spawnInterval);
+        hasBoltBeenRemoved = false;
+        hasFireBeenRemoved = false;
+        validPickups = new List<GameObject>(pickupPrefabs);
     }
 
+    void Update(){
+        if (boltCount.value == boltCount.upperBound && !hasBoltBeenRemoved)
+        {
+            for (int i = 0; i < validPickups.Count; i++){
+                if (validPickups[i].name == "BoltPickup")
+                {
+                    validPickups.RemoveAt(i);
+                    break;
+                }
+            }
+            hasBoltBeenRemoved = true;
+        }
+        if (fireRate.value == fireRate.lowerBound && !hasFireBeenRemoved)
+        {
+            for (int i = 0; i < validPickups.Count; i++){
+                if (validPickups[i].name == "FireRatePickup")
+                {
+                    validPickups.RemoveAt(i);
+                    break;
+                }
+            }
+            hasFireBeenRemoved = true;
+        }
+    }
+
+
+/*HOW SpawnRandomPickup() DETERMINES WHAT TO SPAWN
+    1: pickupPrefabs[] is a Gamobject array containing all the pickup prefabs to potentially spawn
+    2: validPickups is a list that starts with all the pickupPrefabs items in it
+    3: If the bolt count ever reaches its max, it is removed from the validPickups list
+    4: if the fire rate ever reaches its min, it is removed from the validPickups list
+    5: SpawnRandomPickup randomly selects an item from the validPickups list and spawns it in at a random z position within range at the top of the screen*/
     void SpawnRandomPickup()
     {
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
-        int pickupIndex = Random.Range(0,pickupPrefabs.Length); //Picks a random UFO from the array
-        Instantiate(pickupPrefabs[pickupIndex], spawnPos, pickupPrefabs[pickupIndex].transform.rotation); //Spawn an indexed UFO from the array
+        int pickupIndex = Random.Range(0,validPickups.Count); //Picks a random pickup from the validPickups list
+        Instantiate(validPickups[pickupIndex], spawnPos, validPickups[pickupIndex].transform.rotation); //Spawns the randomly selected object to a random z position at the top of the screen
     }
 }
--- Unit 6/UFO TopDown/Assets/Scripts/RunIntoSomething.cs
+++ Unit 6/UFO TopDown/Assets/Scripts/RunIntoSomething.cs
@@ -10,7 +10,14 @@ public class RunIntoSomething : MonoBehaviour
     public GameObject fireRatePickup;
     public FloatScriptableObject fireRate;
     public GameObject fireRateText;
+    public GameObject pointPickup;
+    private ScoreManager scoreManager;
+    public int pickupReward;
+    public GameObject pointPickupText;
 
+    void Awake(){
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); //finds ScoreManager GameObject to reference its ScoreManager script component
+    }
 
     private void OnTriggerEnter(Collider other){
 //if the objet is a boltpickup object, add 1 to the bolt count and destroy the pickup
@@ -18,14 +25,21 @@ public class RunIntoSomething : MonoBehaviour
         {
             boltCount.addValue(1);
             Instantiate(boltPickupText,transform.position,Quaternion.Euler(90,0,0));
-            print("Bolt count is " + boltCount.value);
             Destroy(other.gameObject);
         }
+//if the object is a fireRatePickup object, decrease FireRate (decreases the time between shots) and destroy the pickup
         else if (other.gameObject.name == fireRatePickup.name + "(Clone)" || other.gameObject.name == fireRatePickup.name)
         {
             fireRate.addValue(-0.1f);
             Instantiate(fireRateText,transform.position,Quaternion.Euler(90,0,0));
-            print("Fire rate is " + fireRate.value);
+            Destroy(other.gameObject);
+        }
+//if the object is a pointPickup object, increase the score
+        else if (other.gameObject.name == pointPickup.name + "(Clone)" || other.gameObject.name == pointPickup.name)
+        {
+            scoreManager.IncreaseScore(pickupReward);
+            GameObject text = Instantiate(pointPickupText,transform.position,Quaternion.Euler(90,0,0));
+            text.GetComponent<TextMesh>().text = "+ " + pickupReward + " points";
             Destroy(other.gameObject);
         }
     }
