cf2365adbf71e4d8df5926cbc69923c26827eaba
Theta Rich <10991478@uvu.edu>
2023-03-27 11:47:11 -0600

    Added text that appears when pickups are grabbed

--- Unit 6/UFO TopDown/Assets/Scripts/RunIntoSomething.cs
+++ Unit 6/UFO TopDown/Assets/Scripts/RunIntoSomething.cs
@@ -6,8 +6,10 @@ public class RunIntoSomething : MonoBehaviour
 {
     public GameObject boltPickup;
     public IntObj boltCount;
+    public GameObject boltPickupText;
     public GameObject fireRatePickup;
     public FloatScriptableObject fireRate;
+    public GameObject fireRateText;
 
 
     private void OnTriggerEnter(Collider other){
@@ -15,12 +17,14 @@ public class RunIntoSomething : MonoBehaviour
         if (other.gameObject.name == boltPickup.name + "(Clone)" || other.gameObject.name == boltPickup.name)
         {
             boltCount.addValue(1);
+            Instantiate(boltPickupText,transform.position,Quaternion.Euler(90,0,0));
             print("Bolt count is " + boltCount.value);
             Destroy(other.gameObject);
         }
         else if (other.gameObject.name == fireRatePickup.name + "(Clone)" || other.gameObject.name == fireRatePickup.name)
         {
             fireRate.addValue(-0.1f);
+            Instantiate(fireRateText,transform.position,Quaternion.Euler(90,0,0));
             print("Fire rate is " + fireRate.value);
             Destroy(other.gameObject);
         }
