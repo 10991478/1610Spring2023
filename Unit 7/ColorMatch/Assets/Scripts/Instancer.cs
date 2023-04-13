2d35a8e9ec25e292da1d4ed6b4e38d0bf92afb39
Theta Rich <10991478@uvu.edu>
2023-04-13 00:07:57 -0600

    Added new instancer functions + vector 3 stuff

--- Unit 7/ColorMatch/Assets/Scripts/Instancer.cs
+++ Unit 7/ColorMatch/Assets/Scripts/Instancer.cs
@@ -3,8 +3,40 @@ using UnityEngine;
 
 public class Instancer : ScriptableObject
 {
-    public void CreateInstance(GameObject obj)
+    public GameObject prefab;
+    private int num = 0;
+
+    public void CreateInstance()
     {
-        Instantiate(obj);
+        Instantiate(prefab);
+    }
+
+    public void CreateInstance(Vector3Obj obj)
+    {
+        Instantiate(prefab, obj.value, Quaternion.identity);
+    }
+
+    public void CreateInstanceFromList(Vector3ObjList obj)
+    {
+        for (int i = 0; i < obj.vector3ObjList.Count; i++)
+        {
+            Instantiate(prefab, obj.vector3ObjList[i].value, Quaternion.identity);
+        }
+    }
+
+    public void CreateInstanceFromListCounting(Vector3ObjList obj)
+    {
+        Instantiate(prefab, obj.vector3ObjList[num].value, Quaternion.identity);
+        num++;
+        if (num == obj.vector3ObjList.Count)
+        {
+            num = 0;
+        }
+    }
+
+    public void CreateInstanceFromListRandomly(Vector3ObjList obj)
+    {
+        num = Random.Range(0,obj.vector3ObjList.Count);
+        Instantiate(prefab, obj.vector3ObjList[num].value, Quaternion.identity);
     }
 }
---
+++ Unit 7/ColorMatch/Assets/Scripts/Vector3ObjList.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+
+public class Vector3ObjList : ScriptableObject
+{
+    public List<Vector3Obj> vector3ObjList;
+}
